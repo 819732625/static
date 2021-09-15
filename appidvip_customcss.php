9f06e3af490505906263b1db9ce245a525fdc354
819732625 <56556161+819732625@users.noreply.github.com>
2021-09-15 14:22:38 +0800

    Add files via upload

---
+++ appidvip_customcss.php
@@ -0,0 +1,35 @@
+::-webkit-scrollbar-thumb,.nicescroll-rails div{background-color:#bcbcbc !important;}
+::selection{background:#51aded;color:#fff}
+body,.cms-bar .col-right.s4 .col-small p:before{color:#666}
+body a,.bg-sitenews i.fa-volume-up,.tab-item-comment i,.tab-item-comment i:before,.catlist_1_of_2 .home-heading a,.widget_tinsitestatistic ul li span,.comment-login-pop-click,.comment-info span.cmt-vote i.voted{color:#428bca}
+
+.comment-info span.cmt-vote i.voted,.comment-info span.cmt-vote i:hover{color:#f98181}
+
+.tin-tabs-nav li.active a{border-bottom-color:#428bca}
+.widgettagmore a, #sidebar .widget > h3 a.widgtet-title-more{background-color:#428bca}
+#sidebar #submit-mail:hover{background: #428bca}
+.home-heading a{color: #428bca}
+.home-heading a:hover{font-style:italic}
+body a:hover{color:#51ADED}
+article h3,article h3 a,.tab-item-title a,.slider-text h2 a,  .widget_lxtx_juhe ul.nopic li a,.widget_lxtx_juhe a{color:#000}
+article h3 a:hover,.tab-item-title a:hover,.more-link,.slider-text h2 a:hover,.slider-newest-articles h3 a:hover,  .widget_lxtx_juhe ul.nopic li a:hover,.widget_lxtx_juhe a:hover{color:#51ADED}
+.header-wrap,.pri-nav ul,.pri-nav ul ul,.pri-nav ul ul ul,.user-tabs{background-color:#fff}
+.logo-title a{color:#888}
+
+.pri-nav li > a{font-size:16px}
+.pri-nav li > a,.pri-nav li.current-menu-item a,#focus-us,.focus-content a{color:#333}
+
+
+.pri-nav li a:hover, #focus-us:hover, #focus-us:focus, #focus-slide{background:#f5f5f5}
+.pri-nav li > a:hover,.pri-nav li:hover > a, .pri-nav li.focus > a, #focus-us:hover, #focus-us:focus, .login-yet-click a,.logo-title a:hover{color:#51ADED}
+.pri-nav ul ul,.pri-nav a:hover,.pri-nav li.current-menu-item a{border-bottom-color:#51ADED}
+
+#sidebar .widget > h3 span, .floatwidget h3 span, .multi-border-hl span,.tin-tabs-nav li.active a,.cms-bar .heading-text, .heading-text-blog,.cms-bar .stickys .heading-text-cms.active,span#comments.active, span#comments_quote.active,#ft-wg-sr .widget > h3 span{border-bottom-color:#f98181}
+.tin-tabs-nav li a:hover{color:#f98181}
+@media screen and (max-width: 860px) and (min-width:641px){
+	#flexslider-right{background:#fff}
+}
+@media screen and (max-width: 480px){
+	h3,h3 a{font-size:14px !important;}
+	article p,.content,.content p,.content li,.single-content,.single-text,.sg-cp p,.widget,.widget a, .comments-main, #single-blog-wrap .area .single-content{font-size:14px !important;}
+}
